d181d8a2bcecaaa001b32495478013aad6a07ece
Adison Cavani <adriansrodon@tuta.io>
2022-11-29 17:52:31 +0100

    Add 06-2022 - zadanie 4.2

---
+++ 06-2022/src/zad42.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class zad42 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		var file = new File("przyklad.txt");
+		var scanner = new Scanner(file);
+		
+		var largestDiff = 0;
+		var largestNum = 0;
+		
+		while(scanner.hasNextInt()) {
+			var number = scanner.nextInt();
+			var inversedNumber = inverseNumber(number);
+			
+			var diff = Math.abs(number - inversedNumber);
+			
+			if (largestDiff < diff) {
+				largestDiff = diff;
+				largestNum = number;
+			}
+		}
+		
+		scanner.close();
+		System.out.println(largestNum + " " + largestDiff);
+	}
+	
+	public static int inverseNumber(int number) {
+		var result = 0;
+		
+		while (number != 0) {
+			var tmp = number % 10;
+			result = result * 10 + tmp;
+			number /= 10;
+		}
+
+		return result;
+	}
+}
