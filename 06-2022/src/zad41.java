f512dd14a599b4a950dd696baf6503d3a16d0a8f
Adison Cavani <adriansrodon@tuta.io>
2022-11-29 17:41:56 +0100

    06-2022 - zadanie 4.1

---
+++ 06-2022/src/zad41.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class zad41 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		var file = new File("przyklad.txt");
+		var scanner = new Scanner(file);
+		
+		while(scanner.hasNextInt()) {
+			var number = scanner.nextInt();
+			var inversedNumber = inverseNumber(number);
+			
+			if (inversedNumber % 17 == 0) {
+				System.out.println(inversedNumber);
+			}
+		}
+		
+		scanner.close();
+	}
+	
+	public static int inverseNumber(int number) {
+		var result = 0;
+		
+		while (number != 0) {
+			var tmp = number % 10;
+			result = result * 10 + tmp;
+			number /= 10;
+		}
+
+		return result;
+	}
+}
